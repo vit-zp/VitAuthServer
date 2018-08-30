822fbce72903c42bef8843539d940d3e6b6fd036
vit-zp <vitaliy.khodak@gmail.com>
2018-08-30 11:23:43 +0300

    very first commit - app isn't running and up

---
+++ src/main/java/org/vit/vitauthserver/VitAuthServerApplication.java
@@ -0,0 +1,12 @@
+package org.vit.vitauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitAuthServerApplication.class, args);
+    }
+}
