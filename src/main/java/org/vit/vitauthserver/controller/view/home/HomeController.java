d72664b56cad0d9762c7e42567d3c7ead82b87a1
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-08-30 13:38:37 +0300

    add thymeleaf support; add first page and controller

---
+++ src/main/java/org/vit/vitauthserver/controller/view/home/HomeController.java
@@ -0,0 +1,17 @@
+package org.vit.vitauthserver.controller.view.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
+    public String welcomePage(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "home";
+    }
+}
