0b1f6b280815918d2e7864636a9513149a5ecc4e
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-08-30 16:34:10 +0300

    repository: added stuff related to REST and DB

--- src/main/java/org/vit/vitauthserver/UserRepository.java
+++ src/main/java/org/vit/vitauthserver/UserRepository.java
@@ -1,10 +1,15 @@
 package org.vit.vitauthserver;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.vit.vitauthserver.model.user.User;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
+@RepositoryRestResource(path = "/users")
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUserName(String userName);
--- src/main/java/org/vit/vitauthserver/repository/role/RoleRepository.java
+++ src/main/java/org/vit/vitauthserver/repository/role/RoleRepository.java
@@ -1,9 +1,14 @@
 package org.vit.vitauthserver.repository.role;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.vit.vitauthserver.model.role.Role;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
+@RepositoryRestResource(path = "/roles")
 public interface RoleRepository extends CrudRepository<Role, Long> {
 }
