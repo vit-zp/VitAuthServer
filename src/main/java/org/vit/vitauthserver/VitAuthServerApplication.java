25c702e64a439ffa196905027257218210a1440c
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-08-31 09:53:30 +0300

    application: enable security

--- src/main/java/org/vit/vitauthserver/VitAuthServerApplication.java
+++ src/main/java/org/vit/vitauthserver/VitAuthServerApplication.java
@@ -2,9 +2,8 @@ package org.vit.vitauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class VitAuthServerApplication {
 
     public static void main(String[] args) {
