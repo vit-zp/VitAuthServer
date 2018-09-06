393a1187bb6b0d63a0e77ad0ff07b3f7e20e8399
Vitalii_Khodak <vitalii.khodak@gmail.com>
2018-09-06 09:41:23 +0300

    logging: цфы added logging stuff for HomeController.java by using LOMBOK

--- src/main/java/org/vit/vitauthserver/controller/view/home/HomeController.java
+++ src/main/java/org/vit/vitauthserver/controller/view/home/HomeController.java
@@ -1,29 +1,33 @@
 package org.vit.vitauthserver.controller.view.home;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@Log4j2
 public class HomeController {
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcomePage(Model model) {
         model.addAttribute("title", "Welcome");
         model.addAttribute("message", "This is welcome page!");
+        log.info("###Welcome page was GET");
         return "home";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(Model model) {
-
+        log.info("###Login page was GET");
         return "login";
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutSuccessfulPage(Model model) {
         model.addAttribute("title", "Logout");
+        log.info("###Logout page was GET");
         return "logout";
     }
 }
